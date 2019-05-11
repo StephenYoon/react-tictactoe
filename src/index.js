32176be1e8fa395676532a9bb37b3bb8286de8fb
Stephen <yoonic@gmail.com>
2019-05-10 22:07:01 -0400

    Converted Square to a function component

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+/* 
+In React, function components are a simpler way to write components that only contain 
+a render method and don’t have their own state. Instead of defining a class which 
+extends React.Component, we can write a function that takes props as input 
+and returns what should be rendered. 
+Function components are less tedious to write than classes, and many components can be expressed this way.
+ */
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
   
 class Board extends React.Component {
