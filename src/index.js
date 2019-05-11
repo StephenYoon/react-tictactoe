04d810579d41e8a107aa96b0cb0271aff9198579
Stephen <yoonic@gmail.com>
2019-05-11 19:47:30 -0400

    More comments

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ function Square(props) {
     </button>
   );
 }
-  
+
+/* 
+This tic tac toe is a board of 3x3 squares or an array of 9 squares.
+Each square starts off as null but can be 'X' or 'O' based on user clicks.
+*/
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -98,15 +102,17 @@ class Game extends React.Component {
 function calculateWinner(squares) {
   // Define an array of tictactoe square indexes for wins
   const lines = [
-    [0, 1, 2], // across
+    [0, 1, 2], // indexes for across
     [3, 4, 5],
     [6, 7, 8],
-    [0, 3, 6], // down
+    [0, 3, 6], // indexes for down
     [1, 4, 7],
     [2, 5, 8],
-    [0, 4, 8], // diagonal
+    [0, 4, 8], // indexes for diagonal
     [2, 4, 6],
   ];
+
+  // Iterate through each win scenario
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
